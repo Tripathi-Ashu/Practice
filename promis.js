f7a3c2449176a0c2e4c6961a0406e7abe9ef326e
null <null>
2026-08-08 16:56:47 +0530

    API handling is compleet

--- promis.js
+++ promis.js
@@ -9,13 +9,46 @@ console.log('Hello ashutosh');
 // })
 
 
-const p2 = p1.then((Response) => {
+// const p2 = p1.then((Response) => {
+//     return Response.json();
+// });
+
+// p2.then((Response) => {
+//     console.log(Response);
+// });
+
+
+// console.log(p2);
+// Promiss chaining
+
+
+fetch('https://api.github.com/users').then((Response) => {
     return Response.json();
+
+    if(!Response.ok){
+        throw new Error('Data is not peresent in server');
+    }
+    return Response.json();
+})
+.then((data) => {
+    //console.log(data);
+   
+    const parent = document.getElementById('first');
+    
+    for(let i=0; i<data.length; i++) {
+    const image =  document.createElement('img');
+    image.src = data[i].avatar_url;
+    image.style.height = '40px';
+    image.style.width ='40px';
+    
+    parent.append(image);
+    }
+
+})
+.catch((error)=> {
+    const parent = document.getElementById('first');
+    parent.textContent = error.message;
 });
 
-p2.then((Response) => {
-    console.log(Response);
-});
 
-
-console.log(p2);
+// reject --> Internet Down , Server Down ,  DNS Down .
