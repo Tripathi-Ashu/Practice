91b213f599a353c9832b8ae9d2c8ceb5f2b0b819
null <null>
2026-08-10 10:50:37 +0530

    change

---
+++ asyn&await.js
@@ -0,0 +1,32 @@
+// asinc function alwes return promis
+
+// async function greet() {
+//     return new Promise((resolve , reject) => {
+//         resolve('Resolve');
+//     })
+// }
+
+// const Response = greet();
+// console.log(Response);
+
+// Response.then((data) => console.log(data))
+// .catch((error) => {
+//     console.log('Error:', error);
+// });
+
+
+
+// async function github() {
+//     const get = await fetch("https://api.github.com/users");
+
+//     console.log(get);
+    
+//     const data = await get.json();
+    
+//     console.log(data);
+    
+// };
+
+// github();
+
+
